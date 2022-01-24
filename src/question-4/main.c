837ceea4b821d0b03f38bab9fe543652a1857a62
João Paulo M <joaopaulopmedeiros@gmail.com>
2022-01-24 14:00:25 -0300

    refactor: var assignment

--- src/question-4/main.c
+++ src/question-4/main.c
@@ -5,19 +5,19 @@
 
 int strEquals(char a[], char b[])  
 {  
-    int flag=1, i=0;  
+    int equals=1, i=0;  
 
     while(a[i]!='\0' && b[i]!='\0')
     {
        if(a[i]!=b[i])  
        {  
-           flag = 0;  
+           equals = 0;  
            break;  
        }  
        i++;  
     }  
 
-    return flag; 
+    return equals; 
 }  
 
 int main(int argc, char *argv[])
